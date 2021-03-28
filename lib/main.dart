81fc5eac115a1cd82c93ca894f271d80c63b37d1
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-28 12:15:44 +0530

    Update main.dart
    removed  example code...

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import './widgets/global_utility_widgets.dart';
-import 'config/config.dart';
-// import './screens/home/ui/home.dart';
-// remove this comment and write the Home Screen code
+import './screens/home/ui/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,41 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PIG',
-      home: MyHomePage(),
-      // replace MyHomePage with Home...
-    );
-  }
-}
-
-// remove the below code
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          vSpacer(factor: 3),
-          heading('heading'),
-          vSpacer(),
-          heading1('heading1'),
-          vSpacer(),
-          heading2('heading2'),
-          vSpacer(),
-          subText('subtext'),
-          vSpacer(),
-          subTitle('subtitle'),
-          vSpacer(),
-          cubeContainer(
-            const Center(
-              child: Text('data'),
-            ),
-          ),
-          vSpacer(),
-          pigArrow(2),
-        ],
-      ),
+      home: Home(),
     );
   }
 }
