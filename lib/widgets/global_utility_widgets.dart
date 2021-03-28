3d17b418ebdf49891a499da5921af2876565beac
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-28 12:16:34 +0530

    Update global_utility_widgets.dart
    added padding container and
    also made all global utility widgets
    to classes

--- lib/widgets/global_utility_widgets.dart
+++ lib/widgets/global_utility_widgets.dart
@@ -1,26 +1,55 @@
 import 'package:flutter/material.dart';
 import '../config/config.dart';
 
-Widget cubeContainer(Widget child, {Color color = white}) {
-  return Container(
-    height: rSHeight(125),
-    width: rSWidth(125),
-    decoration: BoxDecoration(
-      color: color,
-      borderRadius: lightBorderRadius,
-      boxShadow: boxShadow,
-    ),
-    child: child,
-  );
+class CContainer extends StatelessWidget {
+  final Color color;
+  final Widget child;
+
+  const CContainer({Key key, this.child, this.color = white}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: rSHeight(125),
+      width: rSWidth(125),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: lightBorderRadius,
+        boxShadow: boxShadow,
+      ),
+      child: child,
+    );
+  }
 }
 
-Widget pigArrow(double turns) {
-  return RotatedBox(
-    quarterTurns: turns.toInt(),
-    child: const Icon(
-      Icons.arrow_back_ios_rounded,
-      color: primaryColor,
-      size: 20,
-    ),
-  );
+class PigArrow extends StatelessWidget {
+  final double turns;
+
+  const PigArrow({Key key, this.turns}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return RotatedBox(
+      quarterTurns: turns.toInt(),
+      child: const Icon(
+        Icons.arrow_back_ios_rounded,
+        color: primaryColor,
+        size: 20,
+      ),
+    );
+  }
+}
+
+class PContainer extends StatelessWidget {
+  final Widget child;
+
+  const PContainer({Key key, this.child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: hPadding(1),
+      ),
+      child: child,
+    );
+  }
 }
