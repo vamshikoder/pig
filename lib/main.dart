b13dab9e54992e8cfe43fc0f612c8a85064fc670
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-29 14:07:08 +0530

    Update main.dart
    converted to GetMaterialApp for easier
    screen navigations

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 import './screens/home/ui/home.dart';
+import 'screens/home/ui/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      // debugShowMaterialGrid: true,
+      debugShowCheckedModeBanner: false,
       title: 'PIG',
       home: Home(),
     );
