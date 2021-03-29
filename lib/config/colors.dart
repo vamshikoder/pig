5eb7328a86ab6ab3a4279a77f681ad8dbc32f433
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-29 14:06:25 +0530

    Update colors.dart
    changed shadows

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -3,13 +3,24 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color.fromRGBO(123, 237, 141, 1);
 const Color errorColor = Color.fromRGBO(240, 20, 47, 1);
 const Color white = Colors.white;
+const Color black = Colors.black;
+const Color transparent = Colors.transparent;
 const greyColor = Color.fromRGBO(126, 132, 163, 1);
 
 const List<BoxShadow> boxShadow = [
   BoxShadow(
     offset: Offset(0, 5),
-    color: Colors.black26,
-    blurRadius: 30,
-    spreadRadius: 1,
+    color: Colors.black12,
+    blurRadius: 50,
+    spreadRadius: 2,
+  ),
+];
+
+const List<BoxShadow> lightBoxShadow = [
+  BoxShadow(
+    offset: Offset(0, 3),
+    color: Colors.black12,
+    blurRadius: 10,
+    spreadRadius: 0.5,
   ),
 ];
