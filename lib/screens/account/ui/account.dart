6c9499299761fc59824e2b037d285ca762f07da3
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-29 14:07:46 +0530

    shows which screen name
    added a center widget that hold screen name

--- lib/screens/account/ui/account.dart
+++ lib/screens/account/ui/account.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../../../config/config.dart';
+
 class Account extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: heading('account screen'),
+      ),
     );
   }
 }
--- lib/screens/home/ui/home.dart
+++ lib/screens/home/ui/home.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:pig/config/config.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: heading('home screen'),
+      ),
     );
   }
 }
