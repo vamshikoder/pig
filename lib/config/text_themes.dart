f10afac7c8d0298250eff77ffaf35f8590c4aea1
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-30 17:02:55 +0530

    converted to widgets
    converted functions to widgets

--- lib/config/text_themes.dart
+++ lib/config/text_themes.dart
@@ -1,69 +1,115 @@
 import 'package:flutter/material.dart';
 import 'colors.dart';
 
-Widget heading(String text, {Color color = primaryColor}) {
-  return Text(
-    text.toUpperCase(),
-    style: TextStyle(
-      color: color,
-      fontFamily: 'Poppins',
-      fontSize: 30,
-      fontWeight: FontWeight.w900,
-      letterSpacing: 5,
-    ),
-  );
+///[Heading] mainly used for screen name
+class Heading extends StatelessWidget {
+  final String text;
+  final Color color;
+  const Heading(this.text, {Key? key, this.color = primaryColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text.toUpperCase(),
+      style: TextStyle(
+        color: color,
+        fontFamily: 'Poppins',
+        fontSize: 30,
+        fontWeight: FontWeight.w900,
+        letterSpacing: 4,
+      ),
+    );
+  }
 }
 
-Widget heading1(String text, {Color color = greyColor}) {
-  return Text(
-    text.toUpperCase(),
-    style: TextStyle(
-      color: color,
-      fontFamily: 'Poppins',
-      fontSize: 20,
-      fontWeight: FontWeight.bold,
-      letterSpacing: 2.5,
-    ),
-  );
+///[Heading1] used for sub headings or in-body content headings
+class Heading1 extends StatelessWidget {
+  final String text;
+  final Color color;
+  const Heading1(this.text, {Key? key, this.color = greyColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text.toUpperCase(),
+      style: TextStyle(
+        color: color,
+        fontFamily: 'Poppins',
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.5,
+      ),
+    );
+  }
 }
 
-Widget heading2(String text, {Color color = greyColor}) {
-  return Text(
-    text.toUpperCase(),
-    style: TextStyle(
-      color: color,
-      fontFamily: 'Poppins',
-      fontSize: 20,
-      fontWeight: FontWeight.w600,
-      letterSpacing: 2.5,
-    ),
-  );
+///[Heading2] can be used as in-body sub headings
+class Heading2 extends StatelessWidget {
+  final String text;
+  final Color color;
+  const Heading2(this.text, {Key? key, this.color = greyColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text.toUpperCase(),
+      style: TextStyle(
+        color: color,
+        fontFamily: 'Poppins',
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+        letterSpacing: 2.5,
+      ),
+    );
+  }
 }
 
-Widget subText(String text,
-    {Color color = greyColor, double letterSpacing = 1}) {
-  return Text(
-    text,
-    style: TextStyle(
-      color: color,
-      fontFamily: 'Poppins',
-      fontSize: 16,
-      fontWeight: FontWeight.w400,
-      letterSpacing: letterSpacing,
-    ),
-  );
+///[SubText] this is used to show the app contents basic [Text] should be shown
+///using [SubText] widget
+class SubText extends StatelessWidget {
+  final String text;
+  final Color color;
+  final double letterSpacing;
+  const SubText(this.text,
+      {Key? key, this.color = primaryColor, this.letterSpacing = 1})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontFamily: 'Poppins',
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        letterSpacing: letterSpacing,
+      ),
+    );
+  }
 }
 
-Widget subTitle(String text,
-    {Color color = greyColor, double letterSpacing = 2.5}) {
-  return Text(
-    text,
-    style: TextStyle(
-      color: color,
-      fontFamily: 'Poppins',
-      fontSize: 12,
-      fontWeight: FontWeight.w400,
-      letterSpacing: letterSpacing,
-    ),
-  );
+///use [SubTitle] for [Text] that needs less preference.
+class SubTitle extends StatelessWidget {
+  final String text;
+  final Color color;
+  final double letterSpacing;
+  const SubTitle(this.text,
+      {Key? key, this.color = primaryColor, this.letterSpacing = 1})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        letterSpacing: letterSpacing,
+      ),
+    );
+  }
 }
