eabdf5582b85c304303782a1ba1c2251fb27cd7c
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-30 17:04:32 +0530

    Update global_utility_widgets.dart
    added comments for clear understanding of the widgets.

--- lib/widgets/global_utility_widgets.dart
+++ lib/widgets/global_utility_widgets.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import '../config/config.dart';
 
 class CContainer extends StatelessWidget {
+  /// [CContainer] is a basic [Container] with pre configured
+  /// [Size] and [decoration]
+
+  /// [color] specify the color of the [Container]
   final Color color;
+
+  /// [child] is the contents in the [CContainer]
   final Widget child;
 
-  const CContainer({Key key, @required this.child, this.color = white})
+  const CContainer({Key? key, required this.child, this.color = white})
       : super(key: key);
 
   @override
@@ -25,9 +32,14 @@ class CContainer extends StatelessWidget {
 }
 
 class PigArrow extends StatelessWidget {
+  ///[PigArrow] is a [icon] configured so that it can
+  ///be used everywhere while changing [turns] parameter that changes [angle]
+  ///of the icon
+
+  ///[turns] is used to change the angle takes [double]
   final double turns;
 
-  const PigArrow({Key key, @required this.turns}) : super(key: key);
+  const PigArrow({Key? key, required this.turns}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return RotatedBox(
@@ -42,9 +54,14 @@ class PigArrow extends StatelessWidget {
 }
 
 class PContainer extends StatelessWidget {
+  ///[PContainer] is a custom container for [PIG]
+  ///with preconfigured [padding] for [PIG]
+
+  ///[Pcontainer] takes [child] parameter to which
+  ///needs [padding] to be applied
   final Widget child;
 
-  const PContainer({Key key, @required this.child}) : super(key: key);
+  const PContainer({Key? key, required this.child}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -57,19 +74,36 @@ class PContainer extends StatelessWidget {
 }
 
 class Sheet extends StatelessWidget {
+  ///[Sheet] creates a container with preconfigured [Theme] and [Size] settings
+  ///can be configured as needed using the below [parameters]
+
+  /// provide the [Sheet] with the [title] which can used to specify the [contents] below the [sheet]
   final String title;
+
+  /// [height] determines the length of the [sheet].
   final double height;
+
+  /// [arrowVisible] takes a [bool] value which can be shown in case of sheets that needs to be [opened] and [closed]
   final bool arrowVisible;
+
+  /// [turns] determine the angle at which the arrow should be rotated
   final double turns;
+
+  ///[child] this where all the [Widgets] under the [Sheet] should be written
   final Widget child;
 
+  ///[sheetTap] allows to perform some actions on tap of the sheet.
+  final Function? sheetTap;
+
+  //TODO need to implement ontap sheet expansion
   const Sheet({
-    Key key,
-    @required this.title,
-    @required this.arrowVisible,
-    @required this.child,
-    this.turns,
-    this.height,
+    Key? key,
+    required this.title,
+    required this.arrowVisible,
+    required this.child,
+    required this.turns,
+    required this.height,
+    this.sheetTap,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -91,7 +125,7 @@ class Sheet extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                heading1(title, color: black),
+                Heading1(title, color: black),
                 // hSpacer(),
                 if (arrowVisible) PigArrow(turns: turns) else const SizedBox()
               ],
@@ -105,85 +139,106 @@ class Sheet extends StatelessWidget {
   }
 }
 
-class CustomScreen extends StatelessWidget {
-  final List<Widget> rightChildren;
-  final List<Widget> bottomChildren;
-  final String title;
-  final Widget body;
-  final bool backArrowVisible;
-  final Function backArrow;
+class CustomScaffold extends StatelessWidget {
+  ///[CustomScaffold] Should be used as the [body] of [Scaffold]
+  ///this provides custom features which [AppBar] cannot provide.
+  ///the App
 
-  const CustomScreen({
-    Key key,
-    @required this.rightChildren,
-    @required this.body,
-    @required this.title,
-    this.bottomChildren,
-    this.backArrow,
+  /// [head] acts like [AppBar] in [Scaffold] use this to add more elements to the top
+  final List<Widget> head;
+
+  ///[extendedHead] allows you to add more contents below [head]
+  ///this is not necessary to write
+  final List<Widget>? extendedHead;
+
+  ///[title] describes the page that the user is in.
+  final String? title;
+
+  ///[body] is something similar to [body] in [Scaffold]
+  final Widget body;
+
+  ///[backArrowVisible] allows to show arrow so that it is easy to navigate to the proevious [screen]
+  final bool backArrowVisible;
+
+  ///this is a function which tells what to do when the [backArrow] is [tapped]
+  ///this can be used to tell the app to go back to the [previous screen]
+  final Function? backArrow;
+
+  const CustomScaffold({
+    Key? key,
+    required this.head,
+    required this.body,
+    this.title,
+    this.extendedHead,
     this.backArrowVisible = false,
+    this.backArrow,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        vSpacer(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            if (backArrowVisible)
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: hPadding(0.25)),
-                    child: InkWell(
-                      focusColor: transparent,
-                      splashColor: transparent,
-                      highlightColor: transparent,
-                      onTap: () {
-                        backArrow();
-                      },
-                      child: const Icon(
-                        Icons.arrow_back_ios_rounded,
-                        color: primaryColor,
-                        size: 20,
+    final String _title = title ?? '';
+    final List<Widget> _extendedHead = extendedHead ?? const [SizedBox()];
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          vSpacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              if (backArrowVisible)
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: hPadding(0.25)),
+                      child: InkWell(
+                        focusColor: transparent,
+                        splashColor: transparent,
+                        highlightColor: transparent,
+                        onTap: () {
+                          backArrow!();
+                        },
+                        child: const Icon(
+                          Icons.arrow_back_ios_rounded,
+                          color: primaryColor,
+                          size: 20,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: hPadding(0.20)),
-                    child: Container(
-                      color: white,
-                      child: heading('home'),
+                    Padding(
+                      padding: EdgeInsets.only(left: hPadding(0.20)),
+                      child: Container(
+                        color: white,
+                        child: Heading(_title),
+                      ),
                     ),
+                  ],
+                )
+              else
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: hPadding(0.95),
                   ),
-                ],
-              )
-            else
+                  child: Container(
+                    color: white,
+                    child: Heading(_title),
+                  ),
+                ),
               Padding(
-                padding: EdgeInsets.only(
-                  left: hPadding(0.95),
-                ),
-                child: Container(
-                  color: white,
-                  child: heading('home'),
+                padding: EdgeInsets.only(right: hPadding(0.5)),
+                child: Row(
+                  children: head,
                 ),
               ),
-            Padding(
-              padding: EdgeInsets.only(right: hPadding(0.5)),
-              child: Row(
-                children: rightChildren,
-              ),
-            ),
-          ],
-        ),
-        PContainer(
-          child: Column(children: bottomChildren),
-        ),
-        vSpacer(factor: 0.5),
-        body,
-      ],
+            ],
+          ),
+          PContainer(
+            child: Column(children: _extendedHead),
+          ),
+          vSpacer(factor: 0.5),
+          body,
+        ],
+      ),
     );
   }
 }
