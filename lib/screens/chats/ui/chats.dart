e3441b98db91ed1ac4254b0c3b8e4f37c0574214
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-31 15:37:19 +0530

    created file structure
    for
    chats libray notes posts settings about and global controller.

---
+++ lib/screens/chats/ui/chats.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../../../widgets/global_utility_widgets.dart';
+
+class Signin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScaffold(
+      title: 'chats', // remove this line if you don't need a title
+      /// similar to [AppBar] in [Scaffold]
+      head: [
+        //your Widgets
+      ],
+
+      /// similar to [body] in [Scaffold]
+      body: Text('Body Code'),
+    );
+  }
+}
