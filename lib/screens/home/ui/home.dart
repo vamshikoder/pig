174bd8f1e4390002553ef0b7e6efa30f817815cd
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-03-31 15:38:33 +0530

    Update home.dart
    basic UI overview

--- lib/screens/home/ui/home.dart
+++ lib/screens/home/ui/home.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:pig/widgets/global_utility_widgets.dart';
+import 'package:get/get.dart';
+
+import '../../../config/config.dart';
+import '../../../widgets/global_utility_widgets.dart';
+import '../../../widgets/stacked_sheet.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const CustomScaffold(
-      title: 'home', // remove this line if you don't need a title
+    final double _screenHeight = Get.height;
+    return CustomScaffold(
+      title: 'hello', // remove this line if you don't need a title
       /// similar to [AppBar] in [Scaffold]
-      head: [
-        //your Widgets
+      head: const [
+        //your AppBar Widgets
       ],
 
       /// similar to [body] in [Scaffold]
-      body: Text('Body Code'),
+      body:
+
+          ///don't forget to import [stacked_sheets.dart] and [get.dart]
+          StackedSheets(
+        title1: 'title1',
+        title2: 'title2',
+        child1: const SubText('hello from title1'),
+        child2: const SubText('hello from title2'),
+
+        ///[height] factor must be [0.80] if is used as [body]
+        ///if there is any other [Widget] above [StackedSheets] configure [height] accordingly
+        ///don't forget to initialize [final double _screenHeight = Get.height;]
+        height: _screenHeight * 0.50,
+      ),
     );
   }
 }
