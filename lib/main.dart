132672886f2ce419a0f27a6231fad6ffeab979bd
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-04-02 12:13:00 +0530

    wrapped with provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 
 import './screens/home/ui/home.dart';
 import 'screens/home/ui/home.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
