19f367e21d77254ba623d6cc5bc1f9dd6ea036f4
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-04-02 12:18:36 +0530

    made them into consumerwidgets
    to make statemangement easy

--- lib/screens/library/ui/library.dart
+++ lib/screens/library/ui/library.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../widgets/global_utility_widgets.dart';
 
-class Posts extends ConsumerWidget {
+class Library extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     return const CustomScaffold(
-      title: 'posts', // remove this line if you don't need a title
+      title: 'library', // remove this line if you don't need a title
 
       /// similar to [body] in [Scaffold]
       body: Text('Body Code'),
