afddb73148d6ff8698a8da74d205665a9dbf471e
vanhsirki <66583686+vanhsirki@users.noreply.github.com>
2021-05-21 17:31:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/route_manager.dart';
+import 'package:pig/config/colors.dart';
+import 'package:pig/screens/home_screen/ui/home_screen.dart';
+// import 'package:pig/screens/sandbox.dart';
+// import 'package:pig/screens/library_screen/ui/library_screen.dart';
 
-import './data/dummie_books_data.dart';
-import 'screens/library_screen/ui/components/book_screen/book_screen.dart';
+// import './data/dummie_books_data.dart';
+// import 'screens/library_screen/ui/components/book_screen/book_screen.dart';
 // import './screens/home/ui/home.dart';
 
 void main() => runApp(ProviderScope(child: PigApp()));
@@ -16,8 +20,13 @@ class PigApp extends StatelessWidget {
       /// [MaterialApp] extension so that navigation is simple pass [Get.to(Page())] for navigation
       debugShowCheckedModeBanner: false,
       title: 'PIG',
-      home: BookScreen(
-        book: books[0],
+      home: HomeScreen(),
+      theme: ThemeData(
+        primaryColorLight: primaryColor,
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColor,
+        backgroundColor: primaryColor,
+        errorColor: errorColor,
       ),
     );
   }
